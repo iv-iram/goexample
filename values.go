a893182494a825df02d51758853125baecd38037
Iram Abid <iram.abid@instaview.ai>
2024-06-25 00:06:43 +0530

    added values

---
+++ values.go
@@ -0,0 +1,17 @@
+/*Values
+Go has various value types including strings, integers, floats, booleans, etc. Here are a few basic examples.
+Strings, which can be added together with +.   
+Integers and floats.*/
+
+package main
+import "fmt"
+
+func main(){
+	fmt.Println("go"+"lang")
+	fmt.Println("1+1=",1+1)
+	fmt.Println("7.0/3.0 =",7.0/3.0)
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+
+}
