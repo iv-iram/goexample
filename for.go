29477d6019ed815e051378c092c11f67ed9d3d7c
Iram Abid <iram.abid@instaview.ai>
2024-06-25 00:33:25 +0530

    added for

---
+++ for.go
@@ -0,0 +1,36 @@
+//for is Go’s only looping construct. Here are some basic types of for loops.
+package main
+import "fmt"
+
+func main(){
+	i:=1
+	for i <= 3 {
+		fmt.Println(i)  
+		i = i + 1
+	}
+
+	for j:=0;j<3;j++{
+		fmt.Println(j)
+	}
+
+	//Another way of accomplishing the basic “do this N times” iteration is range over an integer.
+
+	for i:= range []int {0,1,2} {
+		fmt.Println("range", i)
+	}
+
+	//for without a condition will loop repeatedly until you break out of the loop or return from the enclosing function.
+
+	for {
+		fmt.Println("loop")
+		break
+	}
+
+
+	for n  := range []int {0,1,2,3,4,5}{
+		if n%2 == 0 {
+			continue
+		}
+		fmt.Println(n)
+	}
+}
