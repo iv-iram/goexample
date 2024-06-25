152f7ae25db3c775e85e6930c12abb1f442f3b0e
Iram Abid <iram.abid@instaview.ai>
2024-06-25 13:44:03 +0530

    added wait-group

---
+++ wait-group/wait-group.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+    "fmt"
+    "sync"
+    "time"
+)
+
+//This WaitGroup is used to wait for all the goroutines launched here to finish. 
+//Note: if a WaitGroup is explicitly passed into functions, it should be done by pointer.
+
+func worker(id int) {
+    fmt.Printf("Worker %d starting\n", id)
+
+    time.Sleep(time.Second)
+    fmt.Printf("Worker %d done\n", id)
+}
+
+func main() {
+
+    var wg sync.WaitGroup
+
+    for i := 1; i <= 5; i++ {
+        wg.Add(1)
+
+        go func() {
+            defer wg.Done()
+            worker(i)
+        }()
+    }
+
+    wg.Wait()
+
+}
